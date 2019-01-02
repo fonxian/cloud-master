6ef9301bb3080a7fe783a7b2e51a4e55714f2731
fonxian <fonxian2015@163.com>
2019-01-03 00:43:34 +0800

    add eureka-client dependency

--- cloud-master/cloud-gateway/src/main/java/com/cloud/gateway/CloudGatewayApplication.java
+++ cloud-master/cloud-gateway/src/main/java/com/cloud/gateway/CloudGatewayApplication.java
@@ -3,12 +3,14 @@ package com.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * Created by fangzhijie on 2019/1/2.
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableZuulProxy
 public class CloudGatewayApplication {
 
 
